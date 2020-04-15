aafa2434ed48bac5ed6d20b52322d8982b9bcd4b
Dawid <dawid.k.kowalski@gmail.com>
2020-04-16 00:25:49 +0200

    Addheader model, and texts for header from database

--- quizapp/models.py
+++ quizapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class HeaderPageTxt(models.Model):
+    # menu texts pl and de
+    menu_simple_question_txt_de = models.CharField(max_length=30)
+    menu_simple_question_txt_pl = models.CharField(max_length=30)
+    menu_full_test_txt_de = models.CharField(max_length=30)
+    menu_full_test_txt_pl = models.CharField(max_length=30)
+    menu_fischerschein_info_txt_de = models.CharField(max_length=30)
+    menu_fischerschein_info_txt_pl = models.CharField(max_length=30)
+    menu_FAQ_txt_de = models.CharField(max_length=30)
+    menu_FAQ_txt_pl = models.CharField(max_length=30)
+    menu_about_project_de = models.CharField(max_length=30)
+    menu_about_project_pl = models.CharField(max_length=30)
+    # hero texts
+    hero_main_txt_de = models.CharField(max_length=80)
+    hero_main_txt_pl = models.CharField(max_length=80)
+    hero_under_txt_de = models.CharField(max_length=80)
+    hero_under_txt_pl = models.CharField(max_length=80)
+
+    def __str__(self):
+        return 'Headerpage texts'
+
+
+
+
+
--- quizapp/views.py
+++ quizapp/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from .models import HeaderPageTxt
 
 def main_page(request):
-    return render(request,'quizapp/main_page.html')
+    header_txt = HeaderPageTxt.objects.all()[:1]
+    header_data = {}
+    header_data['menu_simple_question_txt_de'] = header_txt[0].menu_simple_question_txt_de
+    header_data['menu_simple_question_txt_pl'] = header_txt[0].menu_simple_question_txt_pl
+    header_data['menu_full_test_txt_de'] = header_txt[0].menu_full_test_txt_de
+    header_data['menu_full_test_txt_pl'] = header_txt[0].menu_full_test_txt_pl
+    header_data['menu_fischerschein_info_txt_de'] = header_txt[0].menu_fischerschein_info_txt_de
+    header_data['menu_fischerschein_info_txt_pl'] = header_txt[0].menu_fischerschein_info_txt_pl
+    header_data['menu_FAQ_txt_de'] = header_txt[0].menu_FAQ_txt_de
+    header_data['menu_FAQ_txt_pl'] = header_txt[0].menu_FAQ_txt_pl
+    header_data['menu_about_project_de'] = header_txt[0].menu_about_project_de
+    header_data['menu_about_project_pl'] = header_txt[0].menu_about_project_pl
+    header_data['hero_main_txt_de'] = header_txt[0].hero_main_txt_de
+    header_data['hero_main_txt_pl'] = header_txt[0].hero_main_txt_pl
+    header_data['hero_under_txt_de'] = header_txt[0].hero_under_txt_de
+    header_data['hero_under_txt_pl'] = header_txt[0].hero_under_txt_pl
+    return render(request,'quizapp/main_page.html',header_data)
